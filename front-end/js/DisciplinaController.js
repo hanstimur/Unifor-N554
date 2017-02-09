e633b0e2bcc71eb47711b43d0b11466e90096c44
hanstimur <timur@grupoondas.com.br>
2017-02-09 20:29:38 -0300

    Update DisciplinaController.js

--- front-end/js/DisciplinaController.js
+++ front-end/js/DisciplinaController.js
@@ -7,6 +7,11 @@ app.controller('DisciplinaController', function($scope) {
 		$scope.listaDisciplinas.push($scope.disciplina);
 		$scope.disciplina = {};
 	}
+	$scope.delete = function(disciplina) {
+		var index = $scope.listaDisciplinas.indexOf(disciplina);
+		$scope.listaDisciplinas.splice(index,1);
+
+	}
 
 
 });
